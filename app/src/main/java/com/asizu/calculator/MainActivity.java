f47acc0e25ec65fd4526439739f8e3c557747991
wujinhang <251248963@qq.com>
2015-09-10 00:47:32 +0800

    界面做好了

--- app/src/main/java/com/asizu/calculator/MainActivity.java
+++ app/src/main/java/com/asizu/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.asizu.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,42 +11,79 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button btn_0;
+    Button btn_1;
+    Button btn_2;
+    Button btn_3;
+    Button btn_4;
+    Button btn_5;
+    Button btn_6;
+    Button btn_7;
+    Button btn_8;
+    Button btn_9;
+    Button btn_del;
+    Button btn_division;
+    Button btn_dot;
+    Button btn_equal;
+    Button btn_multiply;
+    Button btn_plus;
+    Button btn_subtract;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //隐藏top bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.index);
+        setContentView(R.layout.calculator);
 
-//        Uri uri = Uri.parse("http://www.baidu.com");
-//        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-//        startActivity(intent);
+        btn_0 = (Button)findViewById(R.id.btn_0);
+        btn_1 = (Button)findViewById(R.id.btn_1);
+        btn_2 = (Button)findViewById(R.id.btn_2);
+        btn_3 = (Button)findViewById(R.id.btn_3);
+        btn_4 = (Button)findViewById(R.id.btn_4);
+        btn_5 = (Button)findViewById(R.id.btn_5);
+        btn_6 = (Button)findViewById(R.id.btn_6);
+        btn_7 = (Button)findViewById(R.id.btn_7);
+        btn_8 = (Button)findViewById(R.id.btn_8);
+        btn_9 = (Button)findViewById(R.id.btn_9);
 
-//        WebView wb = (WebView)findViewById(R.id.wb);
-//        wb.loadUrl("http://www.baidu.com");
-//        wb.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                view.loadUrl(url);
-//                return true;
-//            }
-//        });
+        btn_del = (Button)findViewById(R.id.btn_del);
+        btn_division = (Button)findViewById(R.id.btn_division);
+        btn_dot = (Button)findViewById(R.id.btn_dot);
+        btn_equal = (Button)findViewById(R.id.btn_equal);
+        btn_multiply = (Button)findViewById(R.id.btn_multiply);
+        btn_plus = (Button)findViewById(R.id.btn_plus);
+        btn_subtract = (Button)findViewById(R.id.btn_subtract);
 
-
-//        Button b = (Button)findViewById(R.id.showMessage);
-//        b.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //Toast.makeText(MainActivity.this,"hello world!",Toast.LENGTH_LONG).show();
-//                finish();
-//            }
-//        });
-
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_del.setOnClickListener(this);
+        btn_division.setOnClickListener(this);
+        btn_dot.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        btn_subtract.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        //Toast.makeText(MainActivity.this,"123",Toast.LENGTH_LONG).show();
+        System.out.print("123");
+        Log.d(view.toString(),"123");
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -67,4 +105,5 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
